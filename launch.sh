62005a86ef0db437b0969e007eaef3f60d08709f
Belle <samgarlick@hotmail.co.uk>
2026-07-20 11:04:38 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,6 +4,6 @@ export REPLAYS_PATH="replays"
 python belle_bot/infra/fabric/service.py \
 & python belle_bot/sensors/gps/service.py \
 & python belle_bot/sensors/imu/service.py \
-& python belle_bot/sensors/camera/service.py \
+& python belle_bot/sensors/cameras/service.py \
 & python belle_bot/sensors/microphone/service.py \
 echo "Done"
